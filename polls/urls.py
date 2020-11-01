75f957921ee91a84886cda1cefbb072a04fedb0b
RitoruX <ppittayut.ritoru@gmail.com>
2020-11-01 23:59:55 +0700

    add Authenticated.

--- polls/urls.py
+++ polls/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('<int:question_id>/', views.vote_for_poll, name='detail'),
     path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
     path('<int:question_id>/vote/', views.vote, name='vote'),
+    # path('signup/', views.signup, name='signup')
 ]
